fdb5354a00a2b31da91040a1f5209b62533bc216
user <vrn.max@gmail.com>
2021-11-17 17:41:49 +0300

    add Test cases

---
+++ vrnFramework/vrnFramework.swift
@@ -0,0 +1,22 @@
+//
+//  vrnFramework.swift
+//  vrnFramework
+//
+//  Created by Maksim Voronov on 17.11.2021.
+//
+
+import Foundation
+
+public final class vrnFramework {
+
+    let name = "vrnFramework"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
