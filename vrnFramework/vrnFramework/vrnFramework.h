da3b76569d596044d70e7c598ed2abba01fa99bb
user <vrn.max@gmail.com>
2021-11-17 17:13:35 +0300

    commit
    add project

---
+++ vrnFramework/vrnFramework/vrnFramework.h
@@ -0,0 +1,18 @@
+//
+//  vrnFramework.h
+//  vrnFramework
+//
+//  Created by Maksim Voronov on 17.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for vrnFramework.
+FOUNDATION_EXPORT double vrnFrameworkVersionNumber;
+
+//! Project version string for vrnFramework.
+FOUNDATION_EXPORT const unsigned char vrnFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vrnFramework/PublicHeader.h>
+
+
